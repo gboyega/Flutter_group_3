55b0740e4d02fd3beb1e6767da87380f1314da37
Takuro Gbemisola <mitchelletakuro@gmail.com>
2022-02-13 16:04:18 +0000

    added image and white rounded background

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -16,6 +16,39 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: AppBar(
           title: Text(widget.title),
         ),
-        body: Container());
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                image: new DecorationImage(
+                    image: new AssetImage("images/ScreenOne.jpg"),
+                    fit: BoxFit.fitWidth,
+                    alignment: Alignment.topLeft),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: MediaQuery.of(context).size.height * 0.55,
+                    padding: EdgeInsets.symmetric(horizontal: 30),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(50),
+                        topRight: Radius.circular(50),
+                      ),
+                    ),
+                  
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
